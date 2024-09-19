6fec07a839ca286b967e4528b90a9ff8d36e1956
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-09-19 18:09:53 +0530

    Time: 2 ms (68.12%), Space: 13.8 MB (32.27%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,38 +1,38 @@
+//Approach-(Simple and straight forward convert to minutes and sort)
+//T.C : O(n*2^n)
+//S.C : O(2^n) //The result vector stores all possible outcomes. The number of results is at most exponential in terms of the number of operators, so the space required to store the results is O(2^n).
 class Solution {
 public:
-      vector<int>solve(string s)
-      {
-        vector<int>result;
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]=='*' || s[i]=='-' || s[i]=='+')
-            {
-                vector<int>result_left=solve(s.substr(0,i));
-                vector<int>result_right=solve(s.substr(i+1));
-                
-                for(auto &x:result_left)   
-                for(auto &y:result_right)
-                {
-                    if(s[i]=='+')
-                    result.push_back(x+y);
-                    
-                    else if(s[i]=='-')
-                    result.push_back(x-y);
-                    
-                   else //if(s[i]=='*')
-                    result.push_back(x*y);
-                }
 
+    vector<int> solve(string s) {
+        vector<int> result;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == '+' || s[i] == '-' || s[i] == '*') {
+                vector<int> left_results  = solve(s.substr(0, i));
+                vector<int> right_results = solve(s.substr(i+1));
+
+                for(int &x : left_results) {
+                    for(int &y : right_results) {
+                        if(s[i] == '+') {
+                            result.push_back(x+y);
+                        } else if(s[i] == '-') {
+                            result.push_back(x-y);
+                        } else {
+                            result.push_back(x*y);
+                        }
+                    }
+                }
             }
         }
-                if(result.empty()) 
+
+        if(result.empty()) {
             result.push_back(stoi(s));
-        
+        }
 
         return result;
-      }
-
+    }
     vector<int> diffWaysToCompute(string expression) {
-        return solve(expression);
+         return solve(expression);
     }
 };
