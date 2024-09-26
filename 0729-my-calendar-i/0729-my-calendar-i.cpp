d6f5384a99e1b392f5213ad172a785b92718fe2f
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-09-26 23:50:09 +0530

    Time: 82 ms (53.19%), Space: 41.6 MB (86.63%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,22 @@
+//T.C : O(n^2)
+//S.C : O(n)
+typedef pair<int, int> P;
+class MyCalendar {
+public:
+    
+    vector<P> calendar;
+    MyCalendar() {
+
+    }
+    
+    bool book(int start, int end) {
+        for(int i = 0; i<calendar.size(); i++) {
+            P curr = calendar[i];
+            if(!(end <= curr.first || start >= curr.second))
+                return false;
+        }
+        
+        calendar.push_back({start, end});
+        return true;
+    }
+};
