6c3cc4ba670d86b0d108a3c5b398de5ace11cb6a
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-10-13 00:26:53 +0530

    Time: 270 ms (75.23%), Space: 92.7 MB (93.47%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,23 +1,20 @@
-//Approach (Simply sorting and using heap)
-//T.C : O(nlogn)
-//S.C : O(n)
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        sort(begin(intervals), end(intervals));
+        sort(intervals.begin() ,intervals.end());
 
-        priority_queue<int, vector<int>, greater<int>> pq; //stores end point of groups
+        priority_queue<int,vector<int>,greater<int>>pq;
 
-        for(vector<int>& interval : intervals) {
-            int start = interval[0];
-            int end   = interval[1];
+        for(auto &interval:intervals)
+        {
+            int start=interval[0];
+            int end=interval[1];
 
-            if(!pq.empty() && pq.top() < start) {
-                pq.pop();
-            }
+            if(!pq.empty() && start>pq.top())
+            pq.pop();
+
             pq.push(end);
         }
-
-        return pq.size();
+       return pq.size();
     }
 };
