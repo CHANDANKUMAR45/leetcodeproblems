b9e51fd95bd84bd68f09d55f0858595fc43dab04
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-10-14 23:48:12 +0530

    Time: 181 ms (88.28%), Space: 76 MB (79.55%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+
+        long long sum=0;
+
+        priority_queue<int>pq(nums.begin(),nums.end());//Heapify O(n)
+
+        while(k--)
+        {
+            int maxEl=pq.top();
+            pq.pop();
+
+            sum+=maxEl;
+            maxEl=ceil(maxEl/3.0);
+
+            pq.push(maxEl);
+        }
+
+        return sum;
+        
+    }
+};
